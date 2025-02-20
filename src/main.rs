39b95b64ddc2252140a2dc2dd451cd026ada9f2c
charmful0x <charmful0x@gmail.com>
2025-02-20 13:24:53 +0100

    feat: remove shuttle dep

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 pub mod lassie_client;
 pub mod server;
 pub mod utils;
+
 use crate::server::handlers::{handle_import_cid, handle_status};
+use anyhow::Result;
 use axum::{routing::get, Router};
+use bundler::utils::env_var::get_env_key;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 // let cid = "bafybeib36krhffuh3cupjml4re2wfxldredkir5wti3dttulyemre7xkni";
-
-#[shuttle_runtime::main]
-async fn main(
-    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
-) -> shuttle_axum::ShuttleAxum {
-    // load secrets from Secrets.toml into env var;
-    secrets.into_iter().for_each(|(key, val)| {
-        std::env::set_var(key, val);
-    });
-    let router = Router::new()
+#[tokio::main]
+async fn main() -> Result<()> {
+    let port = get_env_key("PORT".to_string()).unwrap_or("3000".to_string()).parse::<u16>()?;
+    let app = Router::new()
         .route("/", get(handle_status))
         .route("/import/:cid", get(handle_import_cid));
-    Ok(router.into())
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = TcpListener::bind(addr).await?;
+    println!("Server starting on {}", addr);
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
